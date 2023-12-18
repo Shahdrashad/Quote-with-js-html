7ec479fc0f41d4b79dee1e20c99730a208fd7a9b
Shahdrashad <145788216+Shahdrashad@users.noreply.github.com>
2023-12-18 14:35:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+/*alert("hello world");
+document.getElementById("demo").innerHTML="shahd";
+console.log("shahd")*/
+
+
+/*var x=10;
+var y=5;
+var z=15;
+x*=2;
+console.log(x)*/
+
+
+/*var x=Number(prompt("enter num 1"))
+var y=Number(prompt("enter num 2"))
+var z=Number(prompt("enter num 3"))
+
+if(x>y && x>z)++
+{
+   alert(x +"is max")
+}
+else if(y>x && y>z)
+{
+   alert(y + "is max")
+}
+else if(z>x && z>y)
+{
+   alert(z + "is max")
+}
+else{
+   alert("plase try again")
+}*/
+
+/*var letter =prompt("enter your letter")
+if (letter=='a'|| letter=='e'|| letter=='i'|| letter=='o'|| letter=='o')
+{
+   alert("letter is vowel")
+}else
+{
+   alert("letter not vowel")
+}*/
+
+
+/*for(var x=100; x>0; x-=10 )
+{
+   console.log(x);
+}*/
+
+/*var li;
+for(var i=0;i<10;i++)
+{
+   li+='<li>shahd<li>';//concartenation
+}
+console.log(li);
+document.getElementById('myul').innerHTML=li;*/
+
+/*var trs='';
+for(var i=0;i<10;i++)
+{
+   trs+=` <tr>
+   <td>${i}</td>
+   <td>shahd</td>
+   <td>20</td>
+   <td>2</td>
+</tr>`
+}
+console.log(trs)
+document.getElementById('trs').innerHTML=trs;*/
+
+
+/*function welcome()
+{
+   console.log("welcome");
+}
+welcome();//call invoke*/
+
+function sum(n1,n2)
+{
+   console.log(n1+n2)
+}
+sum(10,10)
+
+function div(x,y)
+{
+   console.log(sum(n1,n2))
+}
